b4577a672172855bd0c8613eef2dfd5606b1cf51
Santiago <ssalazarb918@gmail.com>
2025-03-31 15:53:04 -0500

    31/03/2025 actualizacion de turnos sin atender

--- Controllers/getTurnosSinAtender.js
+++ Controllers/getTurnosSinAtender.js
@@ -5,11 +5,23 @@ export const getTurnosSinAtender = async (req, res) => {
     try {
         const turnos = await prisma.turnos.findMany({
             where: {
-            HoraAtencion: null
+                HoraAtencion: null
+            },
+            include: {
+                FormularioRegistro:true
             }
         });
-        console.log(turnos);
-        return res.json(turnos);
+        const turnosSinAtender = {
+            PrimerNombre: turnos.FormularioRegistro.PrimerNombre,
+            SegundoNombre: turnos.FormularioRegistro.SegundoNombre,
+            PrimerApellido: turnos.FormularioRegistro.PrimerApellido,
+            SegundoApellido: turnos.FormularioRegistro.SegundoApellido,
+            NumeroDocumento: turnos.FormularioRegistro.NumeroDocumento,
+            Turno: turnos.Turno
+
+        }
+        console.log(turnosSinAtender);
+        return res.json(turnosSinAtender);
     } catch (error) {
         console.log("Error al enviar los datos de turnos sin atender", error)
         res.status(500).json({ error: error.message });
